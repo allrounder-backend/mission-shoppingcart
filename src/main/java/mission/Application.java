46db3657814d02580e8cfbc291f2149f59c4db9d
kokkumong <jeongkh2001@gmail.com>
2025-07-14 15:27:15 +0900

    <application> 구현

--- src/main/java/mission/Application.java
+++ src/main/java/mission/Application.java
@@ -1,9 +1,21 @@
 package mission;
 
-import api.Console;
+import java.util.ArrayList;
+import java.util.List;
+import mission.controller.ShoppingCartController;
+import mission.view.Input;
 
 public class Application {
+
     public static void main(String[] args) {
-        //TODO: 미션 구현
+        mission.model.LectureList lectureList = new mission.model.LectureList();
+        ShoppingCartController shoppingCartController = new ShoppingCartController();
+        int budget = Input.totalBudget();
+        String input = Input.purchaseList();
+        List<Integer> shoppingListId = new ArrayList<>();
+
+        shoppingCartController.parseListId(shoppingListId, input);
+        int price = shoppingCartController.price(shoppingListId, lectureList);
+        shoppingCartController.comparePrice(budget, price);
     }
 }
