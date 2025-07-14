06991b3f4ce4494349a5551b0d1b8ca20927c7dd
kokkumong <jeongkh2001@gmail.com>
2025-07-14 15:22:56 +0900

    <Model> 완성

---
+++ src/main/java/mission/model/Lecture.java
@@ -0,0 +1,32 @@
+package mission.model;
+
+public class Lecture {
+    private final int lectureID;
+    private final String lectureName;
+    private final String lectureType;
+    private int lecturePrice;
+
+    public Lecture(int lectureID, String lectureName, String lectureType, int lecturePrice) {
+        this.lectureID = lectureID;
+        this.lectureName = lectureName;
+        this.lectureType = lectureType;
+        this.lecturePrice = lecturePrice;
+    }
+
+    public int getLectureID() {
+        return lectureID;
+    }
+
+    public String getLectureName() {
+        return lectureName;
+    }
+
+    public String getLectureType() {
+        return lectureType;
+    }
+
+    public int getLecturePrice() {
+        return lecturePrice;
+    }
+}
+
