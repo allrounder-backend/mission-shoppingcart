b0fe6a3fdc2ff69f5f38ad6784e75fa66d52f1d7
yeunsuh <yeunsuh.kim@gmail.com>
2025-07-23 10:16:22 +0900

    feat : Message

---
+++ src/main/java/mission/message/ExceptionMessage.java
@@ -0,0 +1,11 @@
+package mission.message;
+
+public class ExceptionMessage {
+    public static final String INVALID_MONEY = "올바른 금액을 입력해주세요.";
+    public static final String INVALID_TYPE = "올바른 유형을 입력해주세요.";
+    public static final String INVALID_LECTURE = "올바른 강의를 입력해주세요.";
+    public static final String INVALID_FORMAT = "올바른 형식으로 입력해주세요.";
+    public static final String READING_FILE_ERROR = "파일을 읽다가 에러가 발생했습니다.";
+    public static final String RESULT_FILE_ERROR = "읽은 파일이 잘못되었습니다.";
+    public static final String GEMINI_FUNCTION_ERROR = "Gemini에게서 받은 식이 잘못되었습니다.";
+}
---
+++ src/main/java/mission/message/SystemMessage.java
@@ -0,0 +1,9 @@
+package mission.message;
+
+public class SystemMessage {
+    public static final String INPUT_TOTAL_BUDGET = "총 예산을 입력해 주세요.";
+    public static final String INPUT_TYPE_BUDGET = "유형별 예산을 입력해 주세요.";
+    public static final String INPUT_PURCHASE_LIST = "구입할 강의 목록을 입력해주세요.";
+    public static final String OUTPUT_OVERBUDGET = "예산을 초과했습니다.";
+    public static final String OUTPUT_NOT_OVERBUDGET = "예산을 초과하지 않았습니다.";
+}
