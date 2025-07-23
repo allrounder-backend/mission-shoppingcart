c8233d5cf501195557d6d4bc12f5748a946235cd
yeunsuh <yeunsuh.kim@gmail.com>
2025-07-23 10:19:47 +0900

    trouble : Test 콘솔로 실행하면 잘되는데 테스트가 작동 안하네요..

--- src/test/java/mission/ApplicationTest.java
+++ src/test/java/mission/ApplicationTest.java
@@ -4,15 +4,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import api.Application;
 import api.TestEnvironment;
+import java.io.File;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
 public class ApplicationTest extends TestEnvironment {
     @Test
     void testApplication() {
-        run(List.of("250000", "1, 3, 10, 11, 15, 16"));
+        run(List.of("400000", "DevOps-50000,DBMS-50000,F/W-50000,CS-50000", "1, 6, 11, 12, 14, 15, 16"));
+        System.out.println(output());
+        System.err.println("Current directory: " + new File(".").getAbsolutePath());
+        System.err.println("Files in ./compiled/:");
+        Arrays.stream(new File("compiled").listFiles()).forEach(System.err::println);
         assertTrue(output().contains("예산을 초과했습니다."));
-        assertTrue(output().contains("3,000원"));
+        assertTrue(output().contains("19,300원"));
     }
 
     @Override
